0f471e9fae7ef822136fa60bb2e555c3b7c8c217
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-12-24 13:45:47 +0200

    solve homework#1 at lesson#12 - Output (print)

---
+++ introduction-in-programming-using-cpp/HomeWork1/Program1/p1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "**********************************************\n";
+  std::cout << "Name: Mohammed Qashqesh.\n";
+  std::cout << "Age : 22 Years.\n";
+  std::cout << "City: Hebron.\n";
+  std::cout << "Country: Palestine.\n";
+  std::cout << "**********************************************\n";
+
+  return 0;
+}

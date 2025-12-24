8d9ea58e213a9003f22703898e5c48e64b80f3c4
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-12-24 14:01:01 +0200

    complete lesson#13 Omitting Namespace

---
+++ introduction-in-programming-using-cpp/omitting-namespace/omittingNamespace.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// This line allows us to use standard C++ features
+// like cout, cin, string, etc. without writing std:: every time.
+
+int main()
+{
+  // std::cout << "Mohammed Qashqesh." << std::endl; // Before adding line#2
+  cout << "Mohammed Qashqesh." << endl; // After adding line#2
+
+  return 0;
+}

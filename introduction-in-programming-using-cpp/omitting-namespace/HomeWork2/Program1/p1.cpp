9799e5297584628004dbdfec1fadf7a1beb2fda4
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-12-24 14:04:20 +0200

    solve homework#2 - Omitting Namespace

---
+++ introduction-in-programming-using-cpp/omitting-namespace/HomeWork2/Program1/p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "**********************************************\n";
+  cout << "Name: Mohammed Qashqesh.\n";
+  cout << "Age : 22 Years.\n";
+  cout << "City: Hebron.\n";
+  cout << "Country: Palestine.\n";
+  cout << "**********************************************\n";
+
+  return 0;
+}
---
+++ introduction-in-programming-using-cpp/omitting-namespace/HomeWork2/Program3/p3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "I Love Programming!\n\n";
+  cout << "I promise to be the best developer ever!\n\n";
+  cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
+  cout << "Best Regards.\n";
+  cout << "Mohammed Qsahqesh.\n";
+
+  return 0;
+}

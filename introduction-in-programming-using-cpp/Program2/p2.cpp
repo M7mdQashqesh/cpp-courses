075eb66a7836df101cbe7b4c5c4501ffe3c31dfa
Mohammed Qashqesh <mo.qashqesh@gmail.com>
2025-12-24 13:28:51 +0200

    complete Lesson #12 - Output (print)

---
+++ introduction-in-programming-using-cpp/Program2/p2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+  //=================================={First Block}=================================
+  // std::cout << "Mohammed Qashqesh"; // without insert new line
+  // std::cout << "Mohammed Qashqesh\n"; // with insert new line
+  // std::cout << "Mohammed Qashqesh" << std::endl; // this another way to insert new line
+
+  // std::cout << "Mohammed Qashqesh\n\n"; // insert two new line
+  // std::cout << "Mohammed Qashqesh" << std::endl << std::endl; // this is the second way
+
+  // std::cout << "This is my first c++ program!\n"; // add this line when execute this block
+  //================================================================================
+
+  //=============================={Second Block}=====================================
+  // std::cout << "M1" << "M2" << "M3\n"; // print multi message in the same line
+  // std::cout << "M1 " << "M2 " << "M3";
+
+  // std::cout << "10 + 6 = " << 10 + 6 << std::endl; // you can print expressions
+
+  std::cout << "The result of " << "10 + 5 = " << 10 + 5 << std::endl;
+  //=================================================================================
+
+  return 0;
+}
